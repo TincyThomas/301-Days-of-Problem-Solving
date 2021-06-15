f90e5714b96420df3b53e48431dcab8f63132c8f
Tincy Thomas <52705246+TincyThomas@users.noreply.github.com>
2021-06-15 17:11:29 +0530

    Create Binary Addition + 0 1 0 1.py
    Create a function that takes two numbers and returns their sum as a binary string.
    
    Examples
    add_binary(1, 1) ➞ "10"
    
    add_binary(1, 2) ➞ "11"
    
    add_binary(4, 5) ➞ "1001"
    Notes
    Remember to return the converted result to a string.

---
+++ Binary Addition + 0 1 0 1.py
@@ -0,0 +1,7 @@
+def add_binary(a, b):
+    q = str(bin(a+b))                           # basic addition..converting sum to binary ...converting to string
+    return q[2:]                                # removing first two index elements
+
+
+
+print(add_binary(1,1))Binary Addition + 0 1 0 1
