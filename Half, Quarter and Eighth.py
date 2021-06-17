081b9748a7b18015a352131180c3cbffb58d39ae
Tincy Thomas <52705246+TincyThomas@users.noreply.github.com>
2021-06-17 18:04:17 +0530

    Create Half, Quarter and Eighth.py
    Create a function that takes a number and return a list of three numbers: half of the number, quarter of the number and an eighth of the number.
    
    Examples
    half_quarter_eighth(6) ➞ [3, 1.5, 0.75]
    
    half_quarter_eighth(22) ➞ [11, 5.5, 2.75]
    
    half_quarter_eighth(25) ➞ [12.5, 6.25, 3.125]
    Notes
    The order of the list is: half, quarter, eighth.

---
+++ Half, Quarter and Eighth.py
@@ -0,0 +1,5 @@
+def half_quarter_eighth(n):
+	return [n / 2, n / 4, n / 8]                      # half is by 2..quarter is by 4...eighth is by 8
+
+
+print(half_quarter_eighth(6))                       # Function calling and printing
