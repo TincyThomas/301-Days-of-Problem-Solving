987c1c41d2d6ed4d7ce174e2c32f4ebad3b92193
Tincy Thomas <52705246+TincyThomas@users.noreply.github.com>
2021-06-20 19:59:23 +0530

    Create WordCharWord.py
    Create a function that will put the first argument, a character, between every word in the second argument, a string.
    
    Examples
    add("R", "python is fun") ➞ "pythonRisRfun"
    
    add("#", "hello world!") ➞ "hello#world!"
    
    add("#", " ") ➞ "#"
    Notes
    Make sure there are no spaces between words when returning the function

---
+++ WordCharWord.py
@@ -0,0 +1,7 @@
+def add( char, txt):
+
+    return txt.replace(" ", char)      # replace () takes two argument first what to be replaced with next what new has to be added
+
+            
+
+print(add("T", "my name"))
