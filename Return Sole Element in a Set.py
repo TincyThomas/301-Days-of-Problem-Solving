fcfb3d074ebda904f11eb36e1f21fe94abc090e9
Tincy Thomas <52705246+TincyThomas@users.noreply.github.com>
2021-06-21 22:15:47 +0530

    Create Return Sole Element in a Set.py
    Given a set containing one element, return the element.
    
    Examples
    element_from_set({"edabit"}) ➞ "edabit"
    
    element_from_set({True}) ➞ True
    
    element_from_set({11037}) ➞ 11037
    Notes
    Lists, dictionaries, and other sets won't be elements because sets won't accept any mutable data types as elements.

---
+++ Return Sole Element in a Set.py
@@ -0,0 +1,11 @@
+def element_from_set(s):
+
+    a = ""                              # assigning a dummy string variable
+
+    for i in s:                         # looping elements of s
+
+        a = a+i                         # using concatenation
+
+        return a
+
+print (element_from_set({"edabit"}))
