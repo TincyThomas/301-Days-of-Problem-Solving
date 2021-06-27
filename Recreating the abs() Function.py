074851a5258aef26594f80665be3ff7543df9180
Tincy Thomas <52705246+TincyThomas@users.noreply.github.com>
2021-06-27 15:31:15 +0530

    Create Recreating the abs() Function.py
    The abs() function returns the absolute value of a number. This means it returns a number's positive value. You can think of it as the distance away from zero.
    
    Create a function that recreates this functionality.
    
    Examples
    absolute(-5) ➞ 5
    
    absolute(-3.14) ➞ 3.14
    
    absolute(250) ➞ 250
    Notes
    Tests will only include valid numbers.
    Note that positive numbers will stay positive!
    Don't use the abs() function (it will defeat the purpose of the challenge

---
+++ Recreating the abs() Function.py
@@ -0,0 +1,5 @@
+def absolute(n):
+    return n - (2 * n) if n < 0 else n                        # think  logical! when number is negative adding its double to it makes it a positive number
+
+
+print(absolute(-4))
