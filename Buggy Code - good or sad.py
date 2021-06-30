2be6bb9e4963415245b9e8bda62bec1fdaab134e
Tincy Thomas <52705246+TincyThomas@users.noreply.github.com>
2021-06-30 15:39:19 +0530

    Create Buggy Code - good or sad.py
    The challenge is to try and fix this buggy code, given the inputs True and False. See the examples below for the expected output.
    
    Examples
    has_bugs(True) ➞ "sad days"
    
    has_bugs(False) ➞ "it's a good day"

---
+++ Buggy Code - good or sad.py
@@ -0,0 +1,6 @@
+def has_bugs(buggy_code):
+	if buggy_code == True:              #checking input equals true?
+		return 'sad days'
+	else:
+		return "it's a good day"          #checking input equals false?
+print(has_bugs(False))
