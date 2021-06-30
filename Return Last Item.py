f13b7c235b4ebc3878fbb8bc4c59ecd2e67a0571
Tincy Thomas <52705246+TincyThomas@users.noreply.github.com>
2021-06-30 15:28:26 +0530

    Create Return Last Item.py
    Create a function that returns the last value of the last item in a list or string.
    
    Examples
    last_ind([0, 4, 19, 34, 50, -9, 2]) ➞ 2
    
    last_ind("The quick brown fox jumped over the lazy dog") ➞ "g"
    
    last_ind([]) ➞ None
    Notes
    Lists/strings will be of varying size.
    Return None if list/string is empty.

---
+++ Return Last Item.py
@@ -0,0 +1,4 @@
+def last_ind(lst):
+	l = list(lst)                                   # making input to list
+	return l[-1]                                    # taking out last element
+print(last_ind([0, 4, 19, 34, 50, -9, 2]))
