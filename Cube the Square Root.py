d78527cbef73f2be86d855fe6118de8a00ee89c0
Tincy Thomas <52705246+TincyThomas@users.noreply.github.com>
2021-07-01 22:35:38 +0530

    Create Cube the Square Root.py
    Create a function that takes a number as an argument and returns the square root of that number cubed.
    
    Examples
    cube_squareroot(81) ➞ 729
    
    cube_squareroot(1646089) ➞ 2111932187
    
    cube_squareroot(695556) ➞ 580093704
    Notes
    All numbers will evenly square root, so don't worry about decimal numbers.

---
+++ Cube the Square Root.py
@@ -0,0 +1,6 @@
+def cube_squareroot(num):
+    from math import sqrt                   # importing sqrt from math module
+    return int(sqrt(num * num * num))       # devising fromula
+
+
+print(cube_squareroot(81))
