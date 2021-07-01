1383ee1e9008e823762a19e51597de8b7f8222ed
Tincy Thomas <52705246+TincyThomas@users.noreply.github.com>
2021-07-01 22:19:41 +0530

    Create Indexing with Integer Division.py
    Given an index using INTEGER division and a list, return the value of the list with the given index.
    
    Examples
    value_at([1, 2, 3, 4, 5, 6], 10 // 2) ➞ 6
    
    value_at([1, 2, 3, 4, 5, 6], 8.0 // 2) ➞ 5
    
    value_at([1, 2, 3, 4], 6.535355314 // 2) ➞ 4

---
+++ Indexing with Integer Division.py
@@ -0,0 +1,4 @@
+def value_at(lst, index):
+	l = list(lst)                                         # l is a list of lst elements.
+	return l[int(index)]                                  # l[index]
+print(value_at([1, 2, 3, 4, 5, 6], 8.0 // 2))
