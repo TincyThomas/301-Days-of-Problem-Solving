a2a5f6d8060ad5363e16eca5f4a4a208c08ba559
Tincy Thomas <52705246+TincyThomas@users.noreply.github.com>
2021-07-02 12:59:43 +0530

    Create Format I: Template String.py
    For each challenge of this series you do not need to submit a function. Instead, you need to submit a template string that can be formatted in order to get a certain outcome.
    
    Write a template string according to the following example:
    
    Example
    a = "John"
    b = "Joe"
    c = "Jack"
    template = "yourtemplatestringhere"
    template.format(a, b, c) ➞ "Their names were: John, Joe and Jack."
    Tips
    A template string is a string that uses curly braces {} as a placeholder that can then be formatted:
    
    "hello, my name is {}".format("John") ➞ "hello, my name is John."
    Do not put any value inside {}.
    
    Notes
    Submit a string, not a function.
    Do not change the name of the variable template.

---
+++ Format I: Template String.py
@@ -0,0 +1,6 @@
+
+a = "John"
+b = "Joe"
+c = "Jack"
+template = "hello, I am {}, {} is {}'s childhood friend"      # template for format()
+print(template.format(a, b, c))                               # using format()
