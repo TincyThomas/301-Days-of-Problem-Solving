f0fb7b547c0881f26b1663359fb715c8bce10416
Tincy Thomas <52705246+TincyThomas@users.noreply.github.com>
2021-07-05 08:09:26 +0530

    Create Sort Numbers in Ascending Order.py
    Create a function that takes a list of numbers and returns a new list, sorted in ascending order (smallest to biggest).
    
    Sort numbers list in ascending order.
    If the function's argument is None or an empty list, return an empty list.
    Return a new array of sorted numbers.
    Examples
    sort_nums_ascending([1, 2, 10, 50, 5]) ➞ [1, 2, 5, 10, 50]
    
    sort_nums_ascending([80, 29, 4, -95, -24, 85]) ➞ [-95, -24, 4, 29, 80, 85]
    
    sort_nums_ascending([]) ➞ []
    Notes
    Test input can be positive or negative.

---
+++ Sort Numbers in Ascending Order.py
@@ -0,0 +1,8 @@
+def sort_nums_ascending(lst):
+	lst.sort()                                  # this function can be used to sort either in ASC or DESC, your choice. Here we are using ASC
+	return lst
+
+
+print(sort_nums_ascending([80, 29, 4, -95, 24, 85]))
+
+
