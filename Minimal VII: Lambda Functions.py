1fefabd9a7da95e6760bb8277e6e727d3613db04
Tincy Thomas <52705246+TincyThomas@users.noreply.github.com>
2021-07-06 08:24:33 +0530

    Create Minimal VII: Lambda Functions.py
    In the Code tab you will find a code that is missing a single character in order to pass the tests. However, your goal is to submit a function as minimalist as possible. Use the tips in the tips section below.
    
    Write five adder functions:
    
    add_2(x) should return 2 + x.
    add_3(x) should return 3 + x.
    add_5(x) should return 5 + x.
    add_7(x) should return 7 + x.
    add_11(x) should return 11 + x.
    Tips
    Functions that consists only of a return, can be written as oneliner with a lambda function.
    
    For example, the code:
    
    def are_same(a, b):
        return a == b
    Can be simplified to:
    
    are_same = lambda a, b: a == b
    Bonus
    lambda a, b: a == b is technically an anonymous function. However, it can be assigned to the identifier are_same and it can then be called using are_same().

---
+++ Minimal VII: Lambda Functions.py
@@ -0,0 +1,7 @@
+add_2 = lambda x: 2+x                                 # creating lambda functions
+add_3 = lambda x: 3+x
+add_4 = lambda x: 4+x
+add_5 = lambda x: 5+x
+add_7 = lambda x: 7+x
+add_11 = lambda x: 11+x
+print(add_2(7))                                       # printing function
