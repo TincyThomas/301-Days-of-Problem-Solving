a15f21efe17f782a4e6a43f17bc1a77989760329
Tincy Thomas <52705246+TincyThomas@users.noreply.github.com>
2021-07-06 08:22:00 +0530

    Create Format II: Argument Indices.py
    For each challenge of this series you do not need to submit a function. Instead, you need to submit a template string that can be formatted in order to get a certain outcome.
    
    Write a template string according to the following example:
    
    Example
    a = "John"
    b = "Joe"
    template = "yourtemplatestringhere"
    
    template.format(a, b) ➞ "Joe hit John and then John hit Joe."
    Tips
    Writing a number n inside a place holder will tell .format() to fill in with the nth argument. For example:
    
    "{0} said: Hi, I'm {0}.".format("Monica") ➞ "Monica said: Hi, I'm Monica."
    Notes
    Submit a string, not a function.
    Do not change the name of the variable template.

---
+++ Format II: Argument Indices.py
@@ -0,0 +1,4 @@
+#template = "yourtemplatestringhere"
+
+template = "{0} are worthless however {1} is powerful!!".format("Humans","God")               # Make a template sentence
+print(template)                                                                               # print your creation
