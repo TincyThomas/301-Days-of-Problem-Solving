f5cd6c795b1890bb692ab9df1158542fdae81a28
Tincy Thomas <52705246+TincyThomas@users.noreply.github.com>
2021-07-17 12:44:31 +0530

    Create Recreating a len() Function.py
    Create a function which returns the length of a string, without using len().
    
    Examples
    length("Hello World") ➞ 11
    
    length("Edabit") ➞ 6
    
    length("wash your hands!") ➞ 16
    Notes
    N/A

---
+++ Recreating a len() Function.py
@@ -0,0 +1,6 @@
+def length(s):
+	count = 0                           # assignment
+	for i in s:                         # looping input
+		count = count +1                  # increment count
+	return count                        # total length
+print(length("wash your hands!"))
