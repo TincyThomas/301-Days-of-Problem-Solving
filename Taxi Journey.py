890a8868cc70aba0ca7af5215c16314e0ac4282b
Tincy Thomas <52705246+TincyThomas@users.noreply.github.com>
2021-07-20 16:24:54 +0530

    Create Taxi Journey.py
    A taxi journey costs $3 for the first kilometer travelled. However, all kilometers travelled after that will cost $2 each.
    
    Create a function which returns the distance that the taxi must've travelled, given the cost as a parameter.
    
    Examples
    journey_distance(3) ➞ 1
    # The first kilometer costs $3
    
    journey_distance(9) ➞ 4
    # The first kilometer costs $3 plus the other three kilometers (costing $2 each)
    
    journey_distance(5) ➞ 2
    Notes
    The input tests are valid integers >= 0.
    The output will always be a whole number.
    Remember that you are calculating the distance from the cost, not the other way around!

---
+++ Taxi Journey.py
@@ -0,0 +1,3 @@
+def journey_distance(n):
+	return int(n/2)                 # see the pattern, first km is Rs.3 (which is 2+1) then subsequant will be Rs.2 so taking int of division by 2 yields the result 
+print(journey_distance(3))
