d58dc1339612c81cf8350ee646d0ff32452bbb29
Tincy Thomas <52705246+TincyThomas@users.noreply.github.com>
2021-07-23 16:40:32 +0530

    Create Fix the Error: Mutating Arrays.py
    Suppose I want to define a function that removes the last element of a list each time I call it, but does not mutate the original list. Fix the code so that the results are no longer mutating the list.
    
    def minus_one(lst):
      arr.pop()
      return arr
    Examples
    x = [1, 2, 3, 4, 5]
    minus_one(x) ➞ [1, 2, 3, 4]  # 1st time function is called.
    minus_one(x) ➞ [1, 2, 3]  # 2nd time function is called.
    minus_one(x) ➞ [1, 2]  # 3rd time function is called.
    minus_one(x) ➞ [1]  # 4th time function is called.
    
    # What I want instead:
    minus_one(x) ➞ [1, 2, 3, 4]  # 1st time function is called.
    minus_one(x) ➞ [1, 2, 3, 4]  # 2nd time function is called.
    minus_one(x) ➞ [1, 2, 3, 4]  # 3rd time function is called.
    minus_one(x) ➞ [1, 2, 3, 4]  # 4th time function is called.

---
+++ Fix the Error: Mutating Arrays.py
@@ -0,0 +1,10 @@
+# Fix this incorrect code, so that all tests pass!
+def minus_one(lst):
+	arr.pop()
+	return arr
+	
+# Follows working code!
+def minus_one(arr):
+	arr.pop()
+	return arr
+print(minus_one([1, 2, 3, 4, 5]))
