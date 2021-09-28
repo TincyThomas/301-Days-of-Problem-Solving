3032d1a53019e78ad8efbc61b17ce877a7fe2c9d
Tincy Thomas <52705246+TincyThomas@users.noreply.github.com>
2021-09-28 12:35:12 +0530

    Create Sine, Cosine and Tangent.py
    Sine, Cosine and Tangent
    Create three functions that will do three things:
    
    Multiply one number by the sine of another number.
    Multiply one number by the COSINE of another number.
    Multiply one number by the tangent of another number.
    In each function, you will be given 2 numbers: x and y. Another important thing to note, the numbers will be in degrees, not radians. That is extremely important. Remember to round the result to 2 decimal places, as well.
    
    Examples
    sine(8, 27) ➞ 8 sin 27 ➞ 3.63
    
    cosine(10, 4) ➞ 10 cos 4 ➞ 9.98
    
    tangent(4, 39) ➞ 4 tan 39 ➞ 3.24
    Notes
    The math module will be given to you in the code.
    Follow my Instagram: @thatpythoncoder

---
+++ Sine, Cosine and Tangent.py
@@ -0,0 +1,13 @@
+import math
+def sine(x, y):
+    return round(x * math.sin(y),2)
+
+def cosine(x, y):
+    return round(x * math.cos(y),2)
+
+
+def tangent(x, y):
+    return round(x * math.tan(y),2)
+
+
+print(cosine(10, 4))
