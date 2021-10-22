351a7a4f80d4b1edfd76b2111ede052f0d4738c4
Tincy Thomas <52705246+TincyThomas@users.noreply.github.com>
2021-10-22 16:00:07 +0530

    Create Format VI: Padding.py
    Format VI: Padding
    For each challenge of this series you do not need to submit a function. Instead, you need to submit a template string that can formatted in order to get a certain outcome.
    
    Write a three templates string according to the following example. All final strings must have a length of 20 characters:
    
    Example
    starry = "yourtemplatestringhere"
    dash = "yourtemplatestringhere"
    money = "yourtemplatestringhere"
    
    starry.format("Starry") ➞ "*******Starry*******"
    dash.format("Dash") ➞ "----------------Dash"
    money.format("Money") ➞ "Money$$$$$$$$$$$$$$$"
    Tips
    You can pad a string with a placeholder in the format {:cdx} where c is the padding character, d is one of three directions (<, ^ or >) and x is the width.
    
    For example:
    
    "Best score{:->15}".format('AAA') ➞ "Best score------------AAA"
    Notes
    Submit a string, not a function.
    Do not change the name of the variables starry, dash and money.
    You can find all the exercises in this series over here.

---
+++ Format VI: Padding.py
@@ -0,0 +1,10 @@
+starry = "{:*^20}"
+dash = "{:-<20}"
+money = "{:$<20}"
+
+print(starry.format("Starry"))
+#➞ "*******Starry*******"
+print(dash.format("Dash"))
+#➞ "----------------Dash"
+print(money.format("Money"))
+#➞ "Money$$$$$$$$$$$$$$$"
