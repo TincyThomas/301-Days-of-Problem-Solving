5c9dbd11b75adeeda3c388ab0554b37ac4ed3c3a
Tincy Thomas <52705246+TincyThomas@users.noreply.github.com>
2021-12-02 08:30:28 +0530

    Create All occurrences of an Element in a List.py
    All Occurrences of an Element in a List
    Create a function that returns the indices of all occurrences of an item in the list.
    
    Examples
    get_indices(["a", "a", "b", "a", "b", "a"], "a") ➞ [0, 1, 3, 5]
    
    get_indices([1, 5, 5, 2, 7], 7) ➞ [4]
    
    get_indices([1, 5, 5, 2, 7], 5) ➞ [1, 2]
    
    get_indices([1, 5, 5, 2, 7], 8) ➞ []
    Notes
    If an element does not exist in a list, return [].
    Lists are zero-indexed.
    Values in the list will be value-types (don't need to worry about nested lists).

---
+++ All occurrences of an Element in a List.py
@@ -0,0 +1,9 @@
+def get_indices(lst, el):
+    count = -1
+    a = []
+    for i in lst:
+        count +=1
+        if i == el:
+            a+=[count]
+    return a
+print(get_indices(["a", "a", "b", "a", "b", "a"], "a"))
